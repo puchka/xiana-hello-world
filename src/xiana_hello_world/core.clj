b54c2675661f3834795fe56fbd43c42e2dbd3300
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2021-08-27 15:32:55 +0400

    first commit

---
+++ src/xiana_hello_world/core.clj
@@ -0,0 +1,21 @@
+(ns framework.app.hello-word
+  (:require
+   ;; mandatory modules to build/run any web application
+   [xiana.core :as xiana]
+   [framework.route.core :as route]
+   [framework.webserver.core :as webserver]))
+
+;; application route definitions
+(def app-routes
+  [["/" {:action
+         #(xiana/ok
+           (-> % (assoc :response
+                        {:status 200 :body "Hello Word!"})))}]])
+
+(defn -main
+  "Application entry point."
+  []
+  ;; setup app routes
+  (route/reset app-routes)
+  ;; start app webserver
+  (webserver/start []))
---
+++ test/xiana_hello_world/core_test.clj
@@ -0,0 +1,7 @@
+(ns xiana-hello-world.core-test
+  (:require [clojure.test :refer :all]
+            [xiana-hello-world.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
